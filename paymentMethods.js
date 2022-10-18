e29fcf7a61a091525a35de66b55e78068cce047f
MountainPineTW <102243572+MountainPineTW@users.noreply.github.com>
2022-10-18 13:20:53 +0800

    deploy

---
+++ paymentMethods.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    key: "ONSITE",
+    name: "Onsite Payment",
+    icon: "desk",
+    excludeCountries: [],
+  },
+  {
+    key: "NEWEBPAY_ATM",
+    name: "ATM",
+    icon: "atm",
+    excludeCountries: [],
+  },
+  {
+    key: "NEWEBPAY_CVS",
+    name: "超商代碼",
+    icon: "store",
+    excludeCountries: [],
+  },
+  {
+    key: "CREDIT_CARD",
+    name: "Credit Card",
+    icon: "credit-card",
+    excludeCountries: ["TW"],
+  },
+];
